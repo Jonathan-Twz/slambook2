9a258587826cb77f517e8c0d84a1e9d668a5942d
Jonathan Tong <wenzhe.t99@berkeley.edu>
2020-05-03 14:05:58 -0700

    init-build

---
+++ ch13/app/CMakeFiles/run_kitti_stereo.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jonathan/slambook2/ch13/app/run_kitti_stereo.cpp" "/home/jonathan/slambook2/ch13/app/CMakeFiles/run_kitti_stereo.dir/run_kitti_stereo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/eigen3"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  "/home/jonathan/3rd_package/Pangolin/include"
+  "/home/jonathan/3rd_package/Pangolin/build/src/include"
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/usr/include/suitesparse"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jonathan/slambook2/ch13/src/CMakeFiles/myslam.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ch13/src/CMakeFiles/myslam.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jonathan/slambook2/ch13/src/backend.cpp" "/home/jonathan/slambook2/ch13/src/CMakeFiles/myslam.dir/backend.cpp.o"
+  "/home/jonathan/slambook2/ch13/src/camera.cpp" "/home/jonathan/slambook2/ch13/src/CMakeFiles/myslam.dir/camera.cpp.o"
+  "/home/jonathan/slambook2/ch13/src/config.cpp" "/home/jonathan/slambook2/ch13/src/CMakeFiles/myslam.dir/config.cpp.o"
+  "/home/jonathan/slambook2/ch13/src/dataset.cpp" "/home/jonathan/slambook2/ch13/src/CMakeFiles/myslam.dir/dataset.cpp.o"
+  "/home/jonathan/slambook2/ch13/src/feature.cpp" "/home/jonathan/slambook2/ch13/src/CMakeFiles/myslam.dir/feature.cpp.o"
+  "/home/jonathan/slambook2/ch13/src/frame.cpp" "/home/jonathan/slambook2/ch13/src/CMakeFiles/myslam.dir/frame.cpp.o"
+  "/home/jonathan/slambook2/ch13/src/frontend.cpp" "/home/jonathan/slambook2/ch13/src/CMakeFiles/myslam.dir/frontend.cpp.o"
+  "/home/jonathan/slambook2/ch13/src/map.cpp" "/home/jonathan/slambook2/ch13/src/CMakeFiles/myslam.dir/map.cpp.o"
+  "/home/jonathan/slambook2/ch13/src/mappoint.cpp" "/home/jonathan/slambook2/ch13/src/CMakeFiles/myslam.dir/mappoint.cpp.o"
+  "/home/jonathan/slambook2/ch13/src/viewer.cpp" "/home/jonathan/slambook2/ch13/src/CMakeFiles/myslam.dir/viewer.cpp.o"
+  "/home/jonathan/slambook2/ch13/src/visual_odometry.cpp" "/home/jonathan/slambook2/ch13/src/CMakeFiles/myslam.dir/visual_odometry.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/eigen3"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  "/home/jonathan/3rd_package/Pangolin/include"
+  "/home/jonathan/3rd_package/Pangolin/build/src/include"
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/usr/include/suitesparse"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
